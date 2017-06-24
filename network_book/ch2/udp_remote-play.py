e70b20c6ede266cc523bb518284f963134341e77
Jeff Wu <makerjeffwu@gmail.com>
2017-06-24 00:44:33 -0700

    working on udp-remote-play

--- network_book/ch2/udp_remote-play.py
+++ network_book/ch2/udp_remote-play.py
@@ -1,10 +1,18 @@
 import random, socket, sys
 from jwxcolors import jwxcolors as jwxc
 
+# GLOBAL VARS
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+MAX = 65535
+PORT = 1060
 
 # SERVER LOGIC
 if len(sys.argv) >= 2 and sys.argv[1] == 'server':
     print jwxc.BLUE + 'initializing server' + jwxc.ENDC
+
+    s.bind(('127.0.0.1', PORT))
+    #TODO: continue here.
+
 
 # CLIENT LOGIC
 elif len(sys.argv) >= 2 and sys.argv[1] == 'client':
